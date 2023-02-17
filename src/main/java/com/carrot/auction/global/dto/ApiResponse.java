f2455bbb3b56aeed0e4c9c2aa70729ca704c7e65
박세준 <sjpark@selab.co.kr>
2023-02-17 16:19:00 +0900

    [ETC] global api response 생성

---
+++ src/main/java/com/carrot/auction/global/dto/ApiResponse.java
@@ -0,0 +1,28 @@
+package com.carrot.auction.global.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
+@RequiredArgsConstructor
+public record ApiResponse<T>(ApiResponseHeader header, Map<String, T> body) {
+
+    private static final int SUCCESS = 200;
+    private static final int FAILED = 500;
+    private static final String SUCCESS_MESSAGE = "SUCCESS";
+    private static final String FAILED_MESSAGE = "서버에서 오류가 발생하였습니다";
+
+    public static <T> ApiResponse<T> success(String name, T body) {
+        Map<String, T> map = new HashMap<>();
+        map.put(name, body);
+
+        return new ApiResponse<>(new ApiResponseHeader(SUCCESS, SUCCESS_MESSAGE), map);
+    }
+
+    public static <T> ApiResponse<T> fail() {
+        return new ApiResponse<>(new ApiResponseHeader(FAILED, FAILED_MESSAGE), null);
+    }
+}
---
+++ src/main/java/com/carrot/auction/global/dto/ApiResponseHeader.java
@@ -0,0 +1,13 @@
+package com.carrot.auction.global.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class ApiResponseHeader {
+    private int code;
+    private String message;
+}
