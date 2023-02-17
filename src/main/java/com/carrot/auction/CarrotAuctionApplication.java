b513e4822f3203e67c31a189d8b19696f72c32c3
박세준 <sjpark@selab.co.kr>
2023-02-17 16:25:12 +0900

    [ETC] CarrotAuctionApplication 서버 시작점 설정

---
+++ src/main/java/com/carrot/auction/CarrotAuctionApplication.java
@@ -0,0 +1,13 @@
+package com.carrot.auction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrotAuctionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarrotAuctionApplication.class, args);
+    }
+
+}
