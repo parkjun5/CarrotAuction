1f2177c1853c258a907aa88bba1c80f829468e0f
박세준 <sjpark@selab.co.kr>
2023-02-21 17:57:47 +0900

    [FEA] 경매장 엔티티에 아이템, 카테고리, 상태 코드 연결

--- src/main/java/com/carrot/auction/domain/auction/domain/entity/AuctionRoom.java
+++ src/main/java/com/carrot/auction/domain/auction/domain/entity/AuctionRoom.java
@@ -2,6 +2,9 @@ package com.carrot.auction.domain.auction.domain.entity;
 
 import com.carrot.auction.domain.account.domain.entity.Account;
 import com.carrot.auction.domain.account.serailizer.AccountSerializer;
+import com.carrot.auction.domain.chat.domain.ChatRoom;
+import com.carrot.auction.domain.item.domain.Category;
+import com.carrot.auction.domain.item.domain.Item;
 import com.carrot.auction.global.domain.BaseEntity;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import jakarta.persistence.*;
@@ -16,7 +19,7 @@ import java.util.Objects;
 public class AuctionRoom extends BaseEntity implements ChatRoom {
 
     @Id @GeneratedValue
-    @Column(name="auction_id")
+    @Column(name="auction_room_id")
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
@@ -27,6 +30,25 @@ public class AuctionRoom extends BaseEntity implements ChatRoom {
     @OneToMany
     @JsonSerialize(using = AccountSerializer.class)
     private List<Account> participants;
+
+    @Embedded
+    private Item item;
+
+    @Enumerated(EnumType.STRING)
+    private Category category;
+
+    @Enumerated(EnumType.STRING)
+    private AuctionStatus auctionStatus;
+
+    @Override
+    public Long sendChat(Long senderId, String message) {
+        return null;
+    }
+
+    @Override
+    public Long readChat(Long readerId) {
+        return null;
+    }
 
     @Override
     public boolean equals(Object o) {
