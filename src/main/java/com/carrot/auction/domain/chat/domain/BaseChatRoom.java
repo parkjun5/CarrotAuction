683c8cc88eedf6f34f2d3ddcbc5b5038f663934e
박세준 <sjpark@selab.co.kr>
2023-02-23 18:23:14 +0900

    [FEA] BaseChatRoom 참가자 추가  default 메소드 추가

--- src/main/java/com/carrot/auction/domain/chat/domain/BaseChatRoom.java
+++ src/main/java/com/carrot/auction/domain/chat/domain/BaseChatRoom.java
@@ -1,6 +1,12 @@
 package com.carrot.auction.domain.chat.domain;
 
+import com.carrot.auction.domain.user.domain.entity.User;
+
 public interface BaseChatRoom {
     Long sendChat(Long senderId, String message);
     Long readChat(Long readerId);
+
+    default void addParticipants(User user) {
+        user.getAuctionRooms().add(null);
+    }
 }
