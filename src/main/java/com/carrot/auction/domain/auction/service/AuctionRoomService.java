eb5aa54be04094b91a2b55ffb02dcb42411c0237
박세준 <sjpark@selab.co.kr>
2023-02-24 18:10:25 +0900

    [ETC] 옵셔널 처리는 서비스레이어에서 처리하게 변경

--- src/main/java/com/carrot/auction/domain/auction/service/AuctionRoomService.java
+++ src/main/java/com/carrot/auction/domain/auction/service/AuctionRoomService.java
@@ -4,10 +4,8 @@ import com.carrot.auction.domain.auction.domain.entity.AuctionRoom;
 import com.carrot.auction.domain.auction.dto.CreateAuctionRequest;
 import com.carrot.auction.global.dto.ApiResponse;
 
-import java.util.Optional;
-
 public interface AuctionRoomService {
-    Optional<AuctionRoom> findAuctionInfoById(Long auctionRoomId);
+    AuctionRoom findAuctionInfoById(Long auctionRoomId);
 
     ApiResponse<Object> createAuctionRoom(CreateAuctionRequest createAuctionRequest);
 }
