4e9077d05baaa86aeb6734b8a322ff688347e92c
박세준 <sjpark@selab.co.kr>
2023-03-13 17:14:32 +0900

    [REF] BID AuctionRoom 에서 분리

--- src/main/java/com/carrot/auction/domain/bid/domain/entity/Bid.java
+++ src/main/java/com/carrot/auction/domain/bid/domain/entity/Bid.java
@@ -1,22 +1,29 @@
-package com.carrot.auction.domain.auction.domain;
+package com.carrot.auction.domain.bid.domain.entity;
 
+import com.carrot.auction.domain.auction.domain.entity.AuctionRoom;
+import com.carrot.auction.global.domain.BaseEntity;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.persistence.Embeddable;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.time.ZonedDateTime;
 
+@Entity
 @Getter
-@Embeddable
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Builder @AllArgsConstructor(access = AccessLevel.PRIVATE)
-@EqualsAndHashCode
-public class Bid {
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Bid extends BaseEntity {
+
+    @Id @GeneratedValue
+    @Column(name = "bid_id")
+    private Long id;
     @Schema(description = "입찰자 명", example = "1")
     private long bidderId;
+    @OneToOne(mappedBy = "bid")
+    private AuctionRoom auctionRoom;
     @Min(value=1_000, message = "천원보다는 비싼 금액을 입력하세요." )
     @Schema(description = "경매가격", example = "5000")
     private int biddingPrice;
@@ -25,14 +32,8 @@ public class Bid {
     @Schema(description = "입찰 시간" ,type = "string", example = "2023-03-08T00:00:00+0900")
     private ZonedDateTime biddingTime;
 
-    public static Bid startPrice(int biddingPrice) {
-        Bid bid = new Bid();
-        bid.biddingPrice = biddingPrice;
-        return bid;
-    }
-
-    public void changeStartPrice(int biddingPrice) {
-        this.biddingPrice = biddingPrice;
+    public void setAuctionRoom(AuctionRoom auctionRoom) {
+        this.auctionRoom = auctionRoom;
     }
 
     public void changeBid(long bidderId, int biddingPrice, ZonedDateTime biddingTime) {
