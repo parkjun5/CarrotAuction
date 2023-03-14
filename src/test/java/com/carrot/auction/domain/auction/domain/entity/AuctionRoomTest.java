0c81c680085b053c4d431e92044187336c9673c8
박세준 <sjpark@selab.co.kr>
2023-03-14 15:02:24 +0900

    [TEST] 테스트 객체 추가

--- src/test/java/com/carrot/auction/domain/auction/domain/entity/AuctionRoomTest.java
+++ src/test/java/com/carrot/auction/domain/auction/domain/entity/AuctionRoomTest.java
@@ -5,22 +5,32 @@ import com.carrot.auction.domain.item.domain.Category;
 import com.carrot.auction.domain.item.domain.Item;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.annotation.Rollback;
 
 
-import static com.carrot.auction.domain.auction.fixture.AuctionFixture.TEST_AUCTION_ROOM;
+import static com.carrot.auction.domain.auction.fixture.AuctionFixture.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class AuctionRoomTest {
 
     @Test
+    @Rollback
     @DisplayName("경매장 정보 변경 테스트")
     void testChangeInfo() {
         //given
-        AuctionRoom auctionRoom = TEST_AUCTION_ROOM;
+        AuctionRoom auctionRoom = TEST_UPDATE_ROOM;
         String oldName = auctionRoom.getName();
         Category notChangeCategory = auctionRoom.getCategory();
         Item galaxyBook = Item.of("갤럭시 북", 100_000, "맥북보다 훨씬 싼 갤럭시 북 안드로이드 개발에 좋아요");
-        AuctionRequest changeRequest = AuctionRequest.builder().name("이름 변경").item(galaxyBook).bidStartPrice(20_000).category(Category.DIGITAL).build();
+        AuctionRequest changeRequest = AuctionRequest.builder()
+                .name("이름 변경")
+                .item(galaxyBook)
+                .password(null)
+                .category(Category.DIGITAL)
+                .limitOfEnrollment(8)
+                .bidStartPrice(20_000)
+                .beginDateTime(BEGIN_TIME)
+                .closeDateTime(CLOSE_TIME).build();
         //when
         auctionRoom.updateAuctionInfo(changeRequest.name(), changeRequest.password(), changeRequest.limitOfEnrollment(),
                 changeRequest.bidStartPrice(), changeRequest.beginDateTime(), changeRequest.closeDateTime());
