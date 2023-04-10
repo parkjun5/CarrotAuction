8de1cd5c18f10de533b407e55ac8631c482d012b
박세준 <sjpark@selab.co.kr>
2023-04-10 13:53:17 +0900

    [FEA] BiddingRule 객체와 어노테이션으로 Validate 사용을 익명객체 사용으로 변경

--- src/main/java/com/carrot/parkjun5/bidrule/application/BiddingRuleValidator.java
+++ src/main/java/com/carrot/parkjun5/bidrule/application/BiddingRuleValidator.java
@@ -1,18 +1,18 @@
 package com.carrot.parkjun5.bidrule.application;
 
+import com.carrot.parkjun5.auction.application.AuctionService;
 import com.carrot.parkjun5.auction.domain.Auction;
+import com.carrot.parkjun5.auction.exception.NotEnoughBiddingPriceException;
 import com.carrot.parkjun5.bid.application.dto.BidRequest;
-import com.carrot.parkjun5.bidrule.application.annotation.BidRuleName;
-import com.carrot.parkjun5.bidrule.application.rule.BiddingChanceRule;
-import com.carrot.parkjun5.bidrule.application.rule.BiddingTargetAmountRule;
-import com.carrot.parkjun5.bidrule.application.rule.BiddingTickIntervalRule;
-import com.carrot.parkjun5.bidrule.application.rule.BiddingTimeLimitRule;
+import com.carrot.parkjun5.bid.exception.AlreadyEndAuctionException;
+import com.carrot.parkjun5.bid.exception.AlreadyUseBidChanceException;
 import com.carrot.parkjun5.bidrule.exception.NonExclusiveRuleTypeException;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
+import java.time.ZonedDateTime;
 import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -20,53 +20,81 @@ import java.util.stream.Collectors;
 @Component
 @RequiredArgsConstructor
 public class BiddingRuleValidator {
-    private static final Map<String, Object> BIDDING_RULES = new HashMap<>();
-    private final ApplicationContext context;
-    private final BiddingChanceRule biddingChanceRule;
-    private final BiddingTargetAmountRule biddingTargetAmountRule;
-    private final BiddingTickIntervalRule biddingTickIntervalRule;
-    private final BiddingTimeLimitRule biddingTimeLimitRule;
+
+    private static final Map<String, BiddingRule> BIDDING_RULES = new HashMap<>();
+
+    private final AuctionService auctionService;
+
+    @FunctionalInterface
+    interface BiddingRule {
+        void validate(BidRequest req, Auction auction, String ruleValue);
+    }
 
     @PostConstruct
     public void setBiddingRules() {
-        BIDDING_RULES.putAll(context.getBeansWithAnnotation(BidRuleName.class));
-    }
-
-    public void validateBidRule(BidRequest req, Auction auction) {
-        auction.getBidRules().forEach(bidRule -> {
-            switch (bidRule.getName()) {
-                case "ChanceRule" -> biddingChanceRule.validate(req, auction, bidRule.getRuleValue());
-                case "TargetAmountRule" -> biddingTargetAmountRule.validate(auction, bidRule.getRuleValue());
-                case "TickIntervalRule" -> biddingTickIntervalRule.validate(req, auction, bidRule.getRuleValue());
-                case "TimeLimitRule" -> biddingTimeLimitRule.validate(auction);
-                default -> throw new NoSuchElementException("일치하는 룰이 존재하지 않습니다.");
+        BIDDING_RULES.put("ChanceRule", (req, auction, ruleValue) -> {
+            int chanceLimit;
+            try {
+                chanceLimit = Integer.parseInt(ruleValue);
+            } catch (NumberFormatException exception) {
+                return;
+            }
+            int numberOfBids = auctionService.getNumberOfBiddersBid(auction.getId(), req.bidderId());
+            if (chanceLimit <= numberOfBids) {
+                throw new AlreadyUseBidChanceException("이미" + chanceLimit + " 번의 입찰 기회를 전부 사용하였습니다.");
             }
         });
+
+        BIDDING_RULES.put("TargetAmountRule", (req, auction, ruleValue) -> {
+            int targetAmount;
+            try {
+                targetAmount = Integer.parseInt(ruleValue);
+            } catch (NumberFormatException exception) {
+                return;
+            }
+            int nowBidPrice = auctionService.findLastBiddingPrice(auction);
+            if (nowBidPrice >= targetAmount) {
+                auction.endAuction();
+            }
+        });
+
+        BIDDING_RULES.put("TickIntervalRule", (req, auction, ruleValue) -> {
+            int tickInterval;
+            try {
+                tickInterval = Integer.parseInt(ruleValue);
+            } catch (NumberFormatException exception) {
+                return;
+            }
+            int nowBidPrice = auctionService.findLastBiddingPrice(auction);
+            int minimumPrice = Auction.getMinimumPrice(nowBidPrice, BigDecimal.valueOf(tickInterval / 100));
+            if (minimumPrice >= req.biddingPrice()) {
+                throw new NotEnoughBiddingPriceException("최소금액 " + minimumPrice + "보다 제시하신 금액보다 낮습니다.");
+            }
+        });
+        BIDDING_RULES.put("TimeLimitRule", (req, auction, ruleValue) -> {
+            boolean isEndAuction = auction.getCloseDateTime().isBefore(ZonedDateTime.now());
+            if (isEndAuction) {
+                auction.endAuction();
+            }
+            throw new AlreadyEndAuctionException("이미 종료된 경매입니다.");
+        });
+
     }
 
-    public void checkExclusiveRuleType(List<Object> biddingRules) {
-        var countSameTypeRule = biddingRules.stream()
-                .map(Object::getClass)
-                .map(obj -> obj.getAnnotation(BidRuleName.class))
-                .map(BidRuleName::value)
+    public void validateByBiddingRule(BidRequest req, Auction auction) {
+        auction.getBidRules().forEach(bidRule
+                -> BIDDING_RULES.get(bidRule.getName()).validate(req, auction, bidRule.getRuleValue()));
+    }
+
+    public void checkExclusiveRuleType(List<String> biddingRules) {
+        var countSameNames = biddingRules.stream()
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
                 .values();
 
-        boolean hasExclusiveRule = countSameTypeRule.stream().anyMatch(rule -> rule > 1);
+        boolean hasExclusiveRule = countSameNames.stream().anyMatch(rule -> rule > 1);
 
         if (hasExclusiveRule) {
             throw new NonExclusiveRuleTypeException("같은 타입의 룰을 중복하여 설정하였습니다.");
         }
     }
-
-    public List<Object> findRuleByName(List<String> ruleNames) {
-        return ruleNames.stream().map(name
-                -> BIDDING_RULES.keySet().stream()
-                .filter(name::equals)
-                .map(BIDDING_RULES::get)
-                .findAny()
-                .orElseThrow(() -> new NoSuchElementException(name + "이름의 규칙이 존재하지 않습니다."))
-        ).toList();
-    }
-
 }
