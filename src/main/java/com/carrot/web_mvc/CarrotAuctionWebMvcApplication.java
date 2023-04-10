3550e07df6bca12120357184d3dc5eeffb356673
박세준 <sjpark@selab.co.kr>
2023-04-10 15:43:52 +0900

    [ETC] 구조 변경 WebMvc와 WebFlux를 패키지 구조로 나눠서

--- src/main/java/com/carrot/web_mvc/CarrotAuctionWebMvcApplication.java
+++ src/main/java/com/carrot/web_mvc/CarrotAuctionWebMvcApplication.java
@@ -1,15 +1,15 @@
-package com.carrot.parkjun5;
+package com.carrot.web_mvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
 @EnableJpaAuditing
-public class CarrotAuctionApplication {
+@SpringBootApplication
+public class CarrotAuctionWebMvcApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CarrotAuctionApplication.class, args);
+        SpringApplication.run(CarrotAuctionWebMvcApplication.class, args);
     }
 
 }
