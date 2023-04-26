d3e087486da2d0c6b2f84b32604f3dfedb6525cd
박세준 <sjpark@selab.co.kr>
2023-04-26 15:51:59 +0900

    [FEA] r2dbc와 H2 데이터베이스 -> mongdb reactive 데이터베이스로 설정 변경
     -mongodb atlas로 설정해두어서 어디서 든지 사용가능!

--- src/main/java/com/carrot/chat/domain/ChatMessageRepository.java
+++ src/main/java/com/carrot/chat/domain/ChatMessageRepository.java
@@ -1,11 +1,12 @@
 package com.carrot.chat.domain;
 
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 
 @Repository
-public interface ChatMessageRepository extends ReactiveCrudRepository<ChatMessage, Long> {
+public interface ChatMessageRepository extends ReactiveMongoRepository<ChatMessage, Long> {
 
     Flux<ChatMessage> findChatMessageById(Long id);
 
