aed9d5a90dc6f5ba8261f71a6128bae9e06c7b0d
박세준 <sjpark@selab.co.kr>
2023-04-27 14:34:07 +0900

    [FEA] 웹 채팅 핸들러 비지니스로직 분리

---
+++ src/main/java/com/carrot/chat/application/ChatMessageService.java
@@ -0,0 +1,136 @@
+package com.carrot.chat.application;
+
+import com.carrot.chat.domain.ChatMessage;
+import com.carrot.chat.domain.ChatMessageRepository;
+import com.carrot.chat.exception.ChatConvertException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpCookie;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.socket.WebSocketSession;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.publisher.Sinks;
+
+import java.util.List;
+import java.util.UUID;
+
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ChatMessageService {
+
+    /**
+     * 제공된 sinks와 chatMessages를 사용하여 ChatWebSocketHandler를 초기화합니다.
+     *
+     * @param sinks        채팅 메시지 처리를 위한 Sinks.Many 인스턴스
+     * @param chatMessages 채팅 메시지 처리를 위한 Flux 인스턴스
+     */
+
+    private static final String JSESSION_ID = "JSESSION_ID";
+    private final ChatMessageRepository chatRoomRepository;
+    private final SequenceService sequenceService;
+    private final ObjectMapper mapper;
+    private final Sinks.Many<Object> chatSink;
+
+    public Flux<ChatMessage> findAll() {
+        return chatRoomRepository.findAll();
+    }
+
+    public Flux<ChatMessage> findChatMessageBySenderId(String senderId) {
+        return chatRoomRepository.findAllBySenderId(senderId);
+    }
+
+    public Mono<ChatMessage> createChatRoom(ChatMessage chatMessage) {
+        return chatRoomRepository.save(chatMessage);
+    }
+
+    public Flux<ChatMessage> findAllByRoomId(Long roomId) {
+        return null;
+    }
+
+    public void saveChatMessage(ChatMessage chatMessage) {
+        chatMessage.setId(sequenceService.generateSeqByName(ChatMessage.SEQUENCE_NAME));
+        chatRoomRepository.save(chatMessage).subscribe();
+    }
+
+    public Mono<ChatMessage> makeTestMessages() {
+        ChatMessage chatMessage = new ChatMessage();
+        chatMessage.setId(sequenceService.generateSeqByName(ChatMessage.SEQUENCE_NAME));
+        chatMessage.setMessage(UUID.randomUUID().toString());
+        chatMessage.setSenderId("발신자");
+        chatMessage.setChatRoomId("최초 방");
+        return chatRoomRepository.save(chatMessage);
+
+    }
+
+    public Mono<Void> deleteAll() {
+        return chatRoomRepository.deleteAll();
+    }
+
+    /**
+     * 연결 후 로그인 검증을 수행합니다.
+     * 세션에서 JSESSION_ID를 확인하여 로그인이 되어 있는지 판단하고,
+     * 로그인이 되어 있다면 채팅방 입장 메시지를 전송합니다.
+     *
+     * @param session WebSocket 세션
+     * @return 로그인이 유효한 경우 true, 그렇지 않으면 false
+     */
+    public boolean afterConnectionValidate(WebSocketSession session) {
+        HttpCookie httpCookie = session.getHandshakeInfo().getCookies().get(JSESSION_ID)
+                .stream().filter(f -> f.getName().equals(JSESSION_ID)).findAny()
+                .orElse(null);
+
+        if (httpCookie == null) return false;
+
+        if (session.isOpen())
+            chatSink.emitNext(serializeChatMessage(httpCookie.getValue(), "님이 채팅방에 입장하였습니다."), Sinks.EmitFailureHandler.FAIL_FAST);
+        return true;
+    }
+
+    public void sendChatMessage(ChatMessage chatMessage) {
+        try {
+            chatSink.tryEmitNext(mapper.writeValueAsString(chatMessage));
+        } catch (JsonProcessingException e) {
+            throw new IllegalArgumentException(chatMessage.toString() ,e);
+        }
+    }
+
+    public void afterConnectionTerminate(WebSocketSession session) {
+        List<HttpCookie> httpCookies = session.getHandshakeInfo().getCookies().get(JSESSION_ID);
+        String value = httpCookies.stream().filter(f -> f.getName().equals(JSESSION_ID)).findAny()
+                .orElseThrow(() -> new IllegalArgumentException("JSESSION_ID 가 존재하지 않습니다."))
+                .getValue();
+        chatSink.emitNext(serializeChatMessage(value, "님이 채팅방에서 나갔습니다!"), Sinks.EmitFailureHandler.FAIL_FAST);
+    }
+
+    /**
+     * 문자열을 ChatMessage 객체로 변환합니다.
+     * 변환 중 오류가 발생하면 ChatConvertException을 발생시킵니다.
+     *
+     * @param payloadAsText JSON 형식의 문자열
+     * @return 변환된 ChatMessage 객체
+     * @throws ChatConvertException 변환 중 오류 발생 시
+     */
+    public ChatMessage toChatMessage(String payloadAsText) {
+        try {
+            return mapper.readValue(payloadAsText, ChatMessage.class);
+        } catch (JsonProcessingException e) {
+            log.error("Chat Convert Exception : " + payloadAsText);
+            throw new ChatConvertException("채팅 값을 변환하다가 에러가 발생하였습니다." ,e);
+        }
+    }
+
+    private String serializeChatMessage(String value, String alertMessage) {
+        ChatMessage chatMessage = new ChatMessage();
+        chatMessage.setMessage(value + alertMessage);
+        try {
+            return mapper.writeValueAsString(chatMessage);
+        } catch (JsonProcessingException e) {
+            throw new IllegalArgumentException(chatMessage.toString() ,e);
+        }
+    }
+}
--- src/main/java/com/carrot/chat/application/ChatWebSocketHandler.java
+++ src/main/java/com/carrot/chat/application/ChatWebSocketHandler.java
@@ -1,10 +1,6 @@
 package com.carrot.chat.application;
 
-
-import com.carrot.chat.domain.ChatMessage;
-import com.carrot.chat.exception.ChatConvertException;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.socket.WebSocketHandler;
@@ -12,31 +8,14 @@ import org.springframework.web.reactive.socket.WebSocketSession;
 import reactor.core.publisher.*;
 import reactor.core.scheduler.Schedulers;
 
-import java.util.*;
-
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class ChatWebSocketHandler implements WebSocketHandler {
-    private static final String JSESSION_ID = "JSESSION_ID";
     public static final String LOGIN_REQUIRED_MESSAGE = "로그인은 필수입니다.";
     private final Flux<Object> chatMessages;
-    private final Sinks.Many<Object> chatSink;
-    private final ObjectMapper mapper;
-    private final ChatService chatService;
-
-    /**
-     * 제공된 sinks와 chatMessages를 사용하여 ChatWebSocketHandler를 초기화합니다.
-     *
-     * @param sinks        채팅 메시지 처리를 위한 Sinks.Many 인스턴스
-     * @param chatMessages 채팅 메시지 처리를 위한 Flux 인스턴스
-     */
-    public ChatWebSocketHandler(Sinks.Many<Object> sinks, Flux<Object> chatMessages, ChatService chatService) {
-        this.chatMessages = chatMessages;
-        this.chatSink = sinks;
-        this.mapper = new ObjectMapper();
-        this.chatService = chatService;
-    }
+    private final ChatMessageService chatMessageService;
 
     /**
      * WebSocket 연결이 수립되면 호출되는 메소드입니다.
@@ -48,7 +27,7 @@ public class ChatWebSocketHandler implements WebSocketHandler {
      */
     @Override
     public Mono<Void> handle( WebSocketSession session) {
-        if (!afterConnectionLoginValidate(session))
+        if (!chatMessageService.afterConnectionValidate(session))
             return session.send(Flux.just(session.textMessage(LOGIN_REQUIRED_MESSAGE))).then();
 
         processReceivedMessages(session);
@@ -60,28 +39,6 @@ public class ChatWebSocketHandler implements WebSocketHandler {
     }
 
     /**
-     * 연결 후 로그인 검증을 수행합니다.
-     * 세션에서 JSESSION_ID를 확인하여 로그인이 되어 있는지 판단하고,
-     * 로그인이 되어 있다면 채팅방 입장 메시지를 전송합니다.
-     *
-     * @param session WebSocket 세션
-     * @return 로그인이 유효한 경우 true, 그렇지 않으면 false
-     */
-    private boolean afterConnectionLoginValidate(WebSocketSession session) {
-        List<String> sessionIds = session.getHandshakeInfo().getHeaders().get(JSESSION_ID);
-        if (sessionIds == null)
-            return false;
-
-        String loginId = sessionIds.stream().findAny()
-                .orElseThrow(() -> new IllegalArgumentException("JSESSION_ID 가 존재하지 않습니다."));
-
-        if (session.isOpen()) {
-            chatSink.emitNext(loginId + "님이 채팅방에 입장하였습니다.", Sinks.EmitFailureHandler.FAIL_FAST);
-        }
-        return true;
-    }
-
-    /**
      * 클라이언트로부터 받은 메시지를 처리합니다.
      * 메시지를 ChatMessage 객체로 변환하고,
      * 종료 시 채팅방 퇴장 메시지를 전송합니다.
@@ -90,32 +47,11 @@ public class ChatWebSocketHandler implements WebSocketHandler {
      */
     private void processReceivedMessages(WebSocketSession session) {
         session.receive()
-                .map(socketMessage -> toChatMessage(socketMessage.getPayloadAsText()))
+                .map(socketMessage -> chatMessageService.toChatMessage(socketMessage.getPayloadAsText()))
                 .publishOn(Schedulers.boundedElastic())
-                .doOnNext(chatSink::tryEmitNext)
-                .doOnTerminate(() -> {
-                    List<String> sessionId = session.getHandshakeInfo().getHeaders().get(JSESSION_ID);
-                    assert sessionId != null;
-                    String userId = sessionId.stream().findAny().orElseThrow(IllegalArgumentException::new);
-                    chatSink.emitNext(userId + "님이 채팅방에서 나갔습니다!", Sinks.EmitFailureHandler.FAIL_FAST);
-                })
-                .subscribe(chatService::saveChatMessage);
+                .doOnNext(chatMessageService::sendChatMessage)
+                .doOnTerminate(() -> chatMessageService.afterConnectionTerminate(session))
+                .subscribe(chatMessageService::saveChatMessage);
     }
 
-    /**
-     * 문자열을 ChatMessage 객체로 변환합니다.
-     * 변환 중 오류가 발생하면 ChatConvertException을 발생시킵니다.
-     *
-     * @param str JSON 형식의 문자열
-     * @return 변환된 ChatMessage 객체
-     * @throws ChatConvertException 변환 중 오류 발생 시
-     */
-    private ChatMessage toChatMessage(String str) {
-        try {
-            return mapper.readValue(str, ChatMessage.class);
-        } catch (JsonProcessingException e) {
-            log.error("Chat Convert Exception : " + str);
-            throw new ChatConvertException("채팅 값을 변환하다가 에러가 발생하였습니다." ,e);
-        }
-    }
 }
