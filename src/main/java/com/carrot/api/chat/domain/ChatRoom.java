a2531649de1a94a9eff3a4c0b8af986559bd2e71
박세준 <sjpark@selab.co.kr>
2023-05-09 18:14:01 +0900

    [FEA] 채팅방 참가자 처리 추가

--- src/main/java/com/carrot/api/chat/domain/ChatRoom.java
+++ src/main/java/com/carrot/api/chat/domain/ChatRoom.java
@@ -1,24 +1,32 @@
 package com.carrot.api.chat.domain;
 
 import com.carrot.api.common.domain.BaseEntity;
+import com.carrot.api.user.domain.User;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @Getter
 @Entity
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ChatRoom extends BaseEntity {
     @Id @GeneratedValue
     @Column(name = "chat_room_id")
     private Long id;
     private String name;
-    @OneToMany(mappedBy = "chatRoom")
+    @OneToMany(mappedBy = "chatRoom",cascade = CascadeType.ALL)
     @Builder.Default
     private Set<ChatRoomParticipation> chatRoomParticipation = new HashSet<>();
+
+    public void setChatRoomParticipation(User user) {
+        this.chatRoomParticipation.add(ChatRoomParticipation.createChatRoomParticipation(user, this));
+    }
+
+    public void changeName(String name) {
+        this.name = name;
+    }
 }
